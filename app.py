3c5a569615d18c3b840d6367556af1844081d1b7
adoroBaY <maxadrap@gmail.com>
2023-02-04 15:20:20 +0200

    add "get content", "avarage",some other fix

--- app.py
+++ app.py
@@ -1,14 +1,57 @@
 from flask import Flask
-
+from webargs import fields
+from webargs.flaskparser import use_args
+from application.astronauts.amount_of_astronauts import astronauts
+from application.average.avarage_params import average_params
+from application.users_generator.users_generator import generate_users
+from application.get_content.read_file import actions_with_file
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hey Arnold'
+@app.route("/")
+def hello():  # put application's code here
+    return (
+        "<h1>WELCOME TO HOME PAGE!</h1>"
+        "<p><a href='./get_content'>GET CONTENT</p>"
+        "<p><a href='./generate-users'>GENERATE USERS</p>"
+        "<p><a href='./space'>SPACE</p>"
+        "<p><a href='./mean'>MEAN</p>"
+    )
 
 
-if __name__ == '__main__':
+@app.route("/get_content")
+def get_content(file=actions_with_file()):
+    with open(file) as data:
+        content = data.read()
+    return f"<h1>GET CONTENT</h1>" f"{content}" f"<br><a href='/'>←BACK</a>"
+
+
+#@app.route("/generate-users")
+#@use_args({"amount": fields.Int(missing=10)}, location="query")
+#def generate_users():
+   # amount = args("amount")
+   # users = generate_users
+   # return
+
+
+@app.route("/space")
+def space(path_to_file=astronauts()):
+    file = path_to_file
+    text_file = open(file)
+    content = text_file.read()
+    return f"<h1>SPACE</h1>" f"{content}" f"<br><a href='/'>←BACK</a>"
+
+
+@app.route("/mean")
+def average() -> str:
+    avr_height, avr_weight = average_params()
+    return (
+        f"<h1>MEAN</h1>"
+        f"<p>Average HEIGHT is: {avr_height} cm.</p>"
+        f"<p>Average WEIGHT is: {avr_weight} kg.</p>"
+        f"<a href='/'>←BACK</a>"
+   )
+
+
+if __name__ == "__main__":
     app.run()
-
-
--- application/average/avarage_params.py
+++ application/average/avarage_params.py
@@ -1,14 +1,19 @@
-import pandas as pd
+import csv
 
 from application.config.paths import FILES_OUTPUT_PATH
 
 
 def average_params():
     path_to_file = FILES_OUTPUT_PATH.joinpath("hw.csv")
-    df = pd.read_csv(path_to_file)
+    with open(path_to_file) as csv_file:
+        height = 0
+        weight = 0
+        reader = csv.DictReader(csv_file)
 
-    avr_height = df["Height(Inches)"].mean()
-    cm_height = round(avr_height * 2.54, 3)
-    avr_weight = df["Weight(Pounds)"].mean()
-    kg_weight = round(avr_weight * 0.45, 3)
-    return f"Average height is: {cm_height} cm.\nAverage weight is: {kg_weight} kg."
+        for row in reader:
+            height += float(row["Height(Inches)"])
+            weight += float(row["Weight(Pounds)"])
+
+        avr_height = round(float(height / int(row["Index"]) * 2.54), 3)  # cm
+        avr_weight = round(float(weight / int(row["Index"]) * 0.45), 3)  # kg
+    return avr_height, avr_weight
