71a7391426cfe9325e1edb34a488f017f5894ef3
adoroBaY <maxadrap@gmail.com>
2023-02-06 13:39:19 +0200

    1st commit from db hw

---
+++ application/services/create_table.py
@@ -0,0 +1,14 @@
+from application.services.db_connection import DBConnection
+
+
+def create_table():
+    with DBConnection() as connection:
+        connection.execute(
+            """
+            CREATE TABLE IF NOT EXISTS phones(
+            pk  INTEGER NOT NULL PRIMARY KEY,
+            contact_name VARCHAR NOT NULL,
+            phone value INTEGER NOT NULL
+            )
+        """
+        )
